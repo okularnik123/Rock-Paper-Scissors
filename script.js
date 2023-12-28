63fdcb1501f74c630fec010fa0da2ee9fa75459b
okularnik123 <okularnik7a@gmail.com>
2023-12-28 21:35:06 +0100

    Created plan to work

---
+++ script.js
@@ -0,0 +1,19 @@
+//Ask player for move
+    //Create prompt and assign it to variable
+    //Check is answer is valid
+        //Convert string to lower case
+        //If valid - continue to computer move
+        //If not valid show alert and try one more time 
+//Create computer move
+    //Create random number from 0 to 2
+    //Create switch with 0 means rock, 1 is paper and 2 is scissors
+//Check who wins
+    //If plater chose rock 
+        //Check is this a tie
+        //Each of combinations
+            //If player have scissors
+                //If computer have rock
+                    //Game Failed
+                //If computer have paper
+                    //Game win
+                
